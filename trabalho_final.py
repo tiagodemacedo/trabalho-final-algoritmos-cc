734a4c3f99c2c4fd4165aaea10eebcf4b13e8f69
tiagodemacedol <tiagodemacedo@outlook.com>
2021-05-10 16:21:08 -0300

    Incluído estoque de produtos.

---
+++ trabalho_final.py
@@ -0,0 +1,14 @@
+# produtos disponíveis, quantidade e preço organizados em formato de dicionário
+
+estoque = {"1 - Pasta de dente", 1000, 12.00,
+           "2 - Feijão", 1000, 6.00,
+           "3 - Arroz", 1000, 4.50,
+           "4 - Macarrão", 1000, 3.75,
+           "5 - Bolacha", 1000, 4.80,
+           "6 - Leite", 1000, 2.90,
+           "7 - Shampoo", 1000, 14.75,
+           "8 - Detergente", 1000, 1.90,
+           "9 - Sabonete", 1000, 3.90,
+           "10 - Sabão em pó", 1000, 16.20}
+
+print(estoque)
