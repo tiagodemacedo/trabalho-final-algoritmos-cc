c599a46c06411cefc6db3ad6f9a86a6e7590c52d
tiagodemacedol <tiagodemacedo@outlook.com>
2021-05-10 21:35:20 -0300

    Incluído função para validar CPF.

--- trabalho_final.py
+++ trabalho_final.py
@@ -1,3 +1,58 @@
+# validação CPF
+def cpf_validar(param1):
+    cpf_string = str(param1)
+    digitos9 = cpf_string[:-2]
+    contador = 10
+    contador_2 = 11
+    soma = 0
+
+    for x in digitos9:
+        x = int(x)
+        soma += x * contador
+        contador -= 1
+    resto_divisao = soma % 11
+
+    digitos10 = None
+
+    if resto_divisao < 2:
+        digitos10 = digitos9 + "0"
+    else:
+        digitos10 = digitos9 + str(11 - resto_divisao)
+
+    soma = 0
+    digitos11 = None
+
+    for x in digitos10:
+        x = int(x)
+        soma += x * contador_2
+        contador_2 -= 1
+    resto_divisao = soma % 11
+
+    if resto_divisao < 2:
+        digitos11 = digitos10 + "0"
+    else:
+        digitos11 = digitos10 + str(11 - resto_divisao)
+
+    copia_digito11 = list(digitos11)
+    copia_cpf_string = list(cpf_string)
+
+    validacao = False
+
+    if copia_digito11[9] == copia_cpf_string[9] and copia_digito11[10] == copia_cpf_string[10]:
+        validacao = True
+    else:
+        validacao
+
+    return validacao
+
+
+cpf = input("Digite o Cpf: ")
+print(cpf_validar(cpf))
+
+# cadastro cliente
+
+# senha cliente
+
 # produtos disponíveis, quantidade e preço organizados em formato de dicionário
 
 estoque = {"1 - Pasta de dente", 1000, 12.00,
@@ -11,4 +66,4 @@ estoque = {"1 - Pasta de dente", 1000, 12.00,
            "9 - Sabonete", 1000, 3.90,
            "10 - Sabão em pó", 1000, 16.20}
 
-print(estoque)
+# print(estoque)
